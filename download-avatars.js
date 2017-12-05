1f442a6417be8c3a3b239548eb50cf757beaabd4
Brandon Samuel <brandonsamuel72@gmail.com>
2017-12-05 01:35:11 +0000

    printed all avatar urls for each user

--- download-avatars.js
+++ download-avatars.js
@@ -1,12 +1,13 @@
 var request = require('request');
 var token = require('./secret');
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
    var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': GITHIB_TOKEN
+      'Authentication': token.GITHUB_TOKEN
     }
   };
 
@@ -16,6 +17,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  if(err) {
+    console.log("Errors:", err);
+  }
+  var contributors = JSON.parse(result);
+  // console.log(contributors);
+  for (var i = 0; i < contributors.length; i++) {
+    console.log(contributors[i].avatar_url);
+  }
+  // console.log("Result:", result);
 });
